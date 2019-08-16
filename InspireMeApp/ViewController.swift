1f21ab9de54c65302b4cec0e824cbbadf3d81c1b
PurpleRiver <ktomogetkak@mail.ru>
2019-08-16 13:06:28 +0500

    Initial Commit

---
+++ InspireMeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InspireMeApp
+//
+//  Created by Blurry Rabbit on 16/08/2019.
+//  Copyright © 2019 Blurry Rabbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
